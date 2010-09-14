a1759fcb2017b80a942d69ccd620634be7cc0395
Eric Asselin <me@ericasselin.com>
2010-09-13 23:55:18 -0400

    Ajout de html5-boilerTemplate

--- footer.php
+++ footer.php
@@ -5,8 +5,23 @@ Copyright &#169; <?php print(date(Y)); ?> <?php bloginfo('name'); ?>
 Blog propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="http://www.ericasselin.com">ericasselin.com</a>
 </p>
 <?php wp_footer(); ?>
-</div>
+</div> <!-- fermeture footer -->
 
 </div> <!-- fermeture wrapper -->
+
+  <!-- Javascript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script>!window.jQuery && document.write('<script src="<?php bloginfo('template_url'); ?>/js/jquery-1.4.2.min.js"><\/script>')</script>
+
+
+  <script src="<?php bloginfo('template_url'); ?>/js/plugins.js?v=1"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/script.js?v=1"></script>
+
+  <!--[if lt IE 7 ]>
+    <script src="<?php bloginfo('template_url'); ?>/js/dd_belatedpng.js?v=1"></script>
+  <![endif]-->
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,17 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?> class="no-js">
 
 <head profile="http://gmpg.org/xfn/11">
+  
+  <!-- www.phpied.com/conditional-comments-block-downloads/ -->
+  <!--[if IE]><![endif]-->
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 <title><?php bloginfo('name') ?><?php if ( is_404() ) : ?> &raquo; <?php _e('Not Found') ?><?php elseif ( is_home() ) : ?> &raquo; <?php bloginfo('description') ?><?php else : ?><?php wp_title() ?><?php endif ?></title>
 
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
-<meta name="Author" lang="fr" content="ericasselin.com - Eric Asselin">
-<meta name="Publisher" content="ericasselin.com - Eric Asselin">
+<meta name="Author" lang="fr" content="ericasselin.com - Eric Asselin" />
+<meta name="Publisher" content="ericasselin.com - Eric Asselin" />
 
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/general.css" type="text/css" media="screen" />
+  <!--  Mobile Viewport Fix
+        j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
+  device-width : Occupy full width of the screen in its current orientation
+  initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
+  maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
+  -->
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
+
+  <!-- CSS : implied media="all" -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css?v=1">
+
+  <!-- For the less-enabled mobile browsers like Opera Mini -->
+  <link rel="stylesheet" media="handheld" href="<?php bloginfo('template_url'); ?>/css/handheld.css?v=1">
+ 
+  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+  <script src="<?php bloginfo('template_url'); ?>/js/modernizr-1.5.min.js"></script>
 
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
@@ -25,5 +50,13 @@
 <?php wp_head(); ?>
   
 </head>
-<body>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+
+<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
+
   <div id="wrapper">
